8d73316d4b9ce4b81d92705457115780b9b4547e
ItO0o <k17014kk@aitech.ac.jp>
2020-09-01 08:37:21 +0900

    layout(nuxt上部)へHeaderの挿入とヘッダーロゴへのリンク埋め込み

--- components/the-Header.vue
+++ components/the-Header.vue
@@ -1,65 +1,41 @@
 <template>
   <div class="header">
-    <v-app id="inspire">
-      <div>
-        <v-card class="header_space" :key="j" :class="`d-flex justify-space-between mb-6`" color="blue lighten-4" :height="100">
-          <v-card :key="s" img="https://picsum.photos/510/300?random" :width="100" class="logo" tile>
-          </v-card>
+    <v-app id="inspire" class="v_header">
+      <v-card
+        class="header_space"
+        :key="j"
+        :class="`d-flex justify-space-between mb-6`"
+        color="blue lighten-4"
+        :height="100"
+        >
+        <nuxt-link to="">
+        <v-img src="https://picsum.photos/510/300?random" width="100" height="100"></v-img>
+        </nuxt-link>
           <div id="chatchPhrase">
             <h1>タイトル(予定地)</h1>
           </div>
-          <v-card :key="t" :width="100" class="menu" outlined tile>
-            <v-btn :height="100" :width="100">Menu</v-btn>
-          </v-card>
+          <v-btn :height="100" :width="100" tile>Menu</v-btn>
         </v-card>
-      </div>
     </v-app>
-    <div class="header_space"></div>
-    <div class="menu"></div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    AcsTop: {
+
+    }
+  }
+}
 </script>
 
 <style scoped>
-/* .logo {
-  background-color:teal;
-} */
-.menu {
-  background-color:teal;
-}
 h1 {
-  color:black;
+  color: black;
   margin: 25px;
 }
-.header_space {
-  background-color:rgb(36, 28, 146);
+.v_header {
+  background-color: white;
 }
-/* .title_logo {
-    height: 150px;
-    width: 150;
-    background-color:teal;
-
-    margin-right: -3px;
-  }
-  .header_space {
-    margin-left: -3px;
-    margin-right: -50px;
-
-    display: inline-block;
-    height: 150px;
-    width: 80%;
-    background-color: rgb(69, 212, 212);
-  }
-  .menu {
-    float: right;
-    height: 150px;
-    width: 150px;
-
-    margin-left: -1px;
-
-    background-color: brown;
-  } */
 </style>>
