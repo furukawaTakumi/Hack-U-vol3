6bc1529cca636cddd72b221bad6d7c30cb536c0f
furukawaTakumi <black3moon88@gmail.com>
2020-08-31 23:40:53 +0900

    [表示追加]Google mapを表示できるようになった。

---
+++ components/GoogleMaps.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      googleMap: null
+    }
+  },
+  mounted () {
+    window.initMap = function () {
+      /* eslint-disable */ // google の変数が未定義エラーとして怒られるため。
+      this.googleMap = new google.maps.Map(document.getElementById('map'), { 
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 4
+      })
+      /* eslint-enable */
+    }
+  },
+  methods: {
+    private () {
+      return {
+      }
+    }
+  },
+  head () {
+    return {
+      script: [
+        { src: `https://maps.googleapis.com/maps/api/js?key=${process.env.NUXT_ENV_GMAP_API_KEY}&callback=initMap`, defer: true }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#map {
+  width: 300px;
+  height: 300px;
+}
+</style>
