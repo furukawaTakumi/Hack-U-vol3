17544e1fb409b2ec19e50f253e8f6626456150c2
ItO0o <k17014kk@aitech.ac.jp>
2020-09-01 09:15:01 +0900

    【途中】セーブポイント

--- components/TheFileSelector.vue
+++ components/TheFileSelector.vue
@@ -1,12 +1,20 @@
 <template>
-
+  <div class="filePikcer">
+      <v-file-input accept="image/*" label="File input"></v-file-input>
+    <a>File Size:{{ fileSize }}</a>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    fileSize: 0
+  }),
+  methods: {
+    GetFileSize () {
 
-  })
+    }
+  }
 }
 
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
+    <TheFileSelector />
     <nuxt />
     <the-footer />
   </div>
 </template>
 
 <script>
+import TheFileSelector from '@/components/TheFileSelector'
 import TheFooter from '@/components/TheFooter'
+
 export default {
   components: {
+    TheFileSelector,
     TheFooter
   }
 }
