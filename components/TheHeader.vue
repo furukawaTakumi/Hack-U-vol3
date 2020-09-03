36c75d29842f2e8d73b1e1cc10a8258dd6839932
ItO0o <k17014kk@aitech.ac.jp>
2020-09-04 06:23:10 +0900

    【変更】タイトルを自動改行させないようにした

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-      <v-card
-        :class="`d-flex justify-space-between mb-6`"
-        color="blue lighten-4"
-        :height="100"
-        tile>
-        <nuxt-link to="">
-          <v-img src="https://picsum.photos/510/300?random" width="100" height="100"></v-img>
-        </nuxt-link>
-        <h1>タイトル(予定地)</h1>
-        <v-btn :height="100" :width="100" tile>Menu</v-btn>
-      </v-card>
+    <v-card
+      :class="`d-flex justify-space-between mb-6`"
+      color="blue lighten-4"
+      :height="100"
+      tile
+    >
+      <nuxt-link to="">
+        <v-img src="https://picsum.photos/510/300?random" width="100" height="100" />
+      </nuxt-link>
+      <h1>タイトル(予定地)</h1>
+      <v-btn :height="100" :width="100" tile>
+        Menu
+      </v-btn>
+    </v-card>
   </div>
 </template>
 
@@ -20,6 +23,7 @@ export default {}
 
 <style scoped>
 h1 {
+  white-space: nowrap;
   color: black;
   margin: 25px;
 }
