4fa17729d22f02e0f0e562822f178975730866c7
furukawaTakumi <black3moon88@gmail.com>
2020-09-04 23:28:38 +0900

    [機能追加]住所入力で緯度経度をマップに設定可能にし、さらに... 緯度経度を外部コンポーネントから取得可能にした。

--- components/GoogleMaps.vue
+++ components/GoogleMaps.vue
@@ -1,27 +1,68 @@
 <template>
-  <div :id="googleMapId" class="google-map" />
+  <div class="map-frame">
+    <div :id="googleMapId" class="google-map" />
+    <v-text-field
+      v-model="addressText"
+      :placeholder="placeholder"
+      @change="geoCording()"
+    />
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    placeholder: {
+      type: String,
+      required: true
+    }
+  },
   data () {
     return {
       googleMapId: this.$uuid.v4(),
-      googleMap: null
+      googleMap: null,
+      geocoder: null,
+      addressText: '',
+      location: { lat: 35.4122, lng: 139.4130 },
+      googleLib: null,
+      marker: null
     }
   },
   mounted () {
     window.addEventListener('load', () => {
       const mapElem = document.getElementById(this.googleMapId)
       /* eslint-disable */ // google の変数が未定義エラーとして怒られるため。
-      this.googleMap = new google.maps.Map(mapElem, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 4
+      this.googleLib = google
+      this.googleMap = new this.googleLib.maps.Map(mapElem, {
+        center: this.location,　zoom: 4
       } )
+      this.geocoder = new this.googleLib.maps.Geocoder()
+      this.marker = new this.googleLib.maps.Marker({ position: this.location })
+      this.setLocation(this.location)
       /* eslint-enable */
     })
   },
   methods: {
+    geoCording () {
+      this.geocoder.geocode({ address: this.addressText }, (response, status) => {
+        if (status === 'OK') {
+          const location = response[0].geometry.location.toJSON()
+          this.setLocation(location)
+        } else {
+          console.error('一致する住所がありません')
+        }
+      })
+    },
+    getLocation () {
+      return this.location
+    },
+    setLocation (location) {
+      this.location = location
+      this.googleMap.setCenter(location)
+      this.marker.setMap(null)
+      this.marker = new this.googleLib.maps.Marker({ position: location })
+      this.marker.setMap(this.googleMap)
+    }
   },
   head () {
     return {
