26a6e1f7b37a60e6491073bf90f56b38361017fa
hihumikan <musecharo@gmail.com>
2020-09-04 12:23:52 +0900

    修正

--- components/GoogleMaps.vue
+++ components/GoogleMaps.vue
@@ -1,32 +1,66 @@
 <template>
-  <div :id="googleMapId" class="google-map" />
+  <html>
+    <div :id="googleMapId" class="google-map" defer />
+      <head>
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+      </head>
+    <body>
+      <div id="floating-panel">
+        <input id="address" type="textbox" value="住所を入力" />
+        <input id="submit" type="button" value="submit"/>
+      </div>
+    <div id="map"></div>
+    </body>
+  <script>
+  "use strict";
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 9,
+    center: {
+      lat: 35.1834122,
+      lng: 137.1130419
+    }
+  });
+  const geocoder = new google.maps.Geocoder();
+  document.getElementById("submit").addEventListener("click", () => {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  const address = document.getElementById("address").value;
+  geocoder.geocode(
+    {
+      address: address
+    },
+    (results, status) => {
+      if (status === "OK") {
+        resultsMap.setCenter(results[0].geometry.location);
+        new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    }
+  );
+}
+
+</script>
+
+</html>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      googleMapId: this.$uuid.v4(),
-      googleMap: null
-    }
-  },
-  mounted () {
-    window.addEventListener('load', () => {
-      const mapElem = document.getElementById(this.googleMapId)
-      /* eslint-disable */ // google の変数が未定義エラーとして怒られるため。
-      this.googleMap = new google.maps.Map(mapElem, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 4
-      } )
-      /* eslint-enable */
-    })
-  },
   methods: {
   },
   head () {
     return {
       script: [
-        { src: `https://maps.googleapis.com/maps/api/js?key=${process.env.NUXT_ENV_GMAP_API_KEY}` }
+        { src: `https://maps.googleapis.com/maps/api/js?key=${process.env.NUXT_ENV_GMAP_API_KEY}&callback=initMap&libraries=&v=weekly"` }
       ]
     }
   }
@@ -34,9 +68,32 @@ export default {
 </script>
 
 <style scoped>
-.google-map {
-  /* 大きさは親要素で指定 */
-  width: 100%;
-  height: 100%;
-}
+     /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+
+      /* Optional: Makes the sample page fill the window. */
+      html,
+      body {
+        height: 400px;
+        width: 450px;
+        margin: 10;
+        padding: 0;
+      }
+
+      #floating-panel {
+        position: absolute;
+        top: 100px;
+        left: 40px;
+        z-index: 5;
+        background-color: #fff;
+        padding: 4px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: "Roboto", "Roboto";
+        line-height: 23px;
+        padding-left: 10px;
+      }
 </style>
