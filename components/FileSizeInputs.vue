4b6a334ed9d18d57f48459e8e2d93eb22f39cdfe
furukawaTakumi <black3moon88@gmail.com>
2020-09-09 01:36:06 +0900

    [追加]仮でファイルサイズをて入力できるようにした。

--- components/FileSizeInputs.vue
+++ components/FileSizeInputs.vue
@@ -3,15 +3,14 @@
     <h5 for="file-size" class="file-size-label">
       送信するファイルサイズを入力してください。
     </h5>
+    <h3 class="file-size-text">{{ fileSizeText }}</h3>
     <div class="file-size-input">
       <input
-        v-model="fileSizeNum"
-        id="file-size"
-        type="number"
+        v-model.number="fileSizeNum"
         class="num-input"
+        placeholder="10000"
         min="0"
       />
-      <div class="unit-type">{{ unitType.text }}</div>
     </div>
   </div>
 </template>
@@ -20,50 +19,65 @@
 export default {
   data() {
     return {
-      fileSizeNum: 1,
-      unitType: { id: 0, text: 'byte', value: 1 },
-      unitTypes: [
-        { id: 0, text: 'byte', value: 1 },
-        { id: 1, text: 'KB', value: 1000 },
-        { id: 2, text: 'MB', value: 1000000 },
-        { id: 3, text: 'GB', value: 1000000000 },
-        { id: 4, text: 'TB', value: 1000000000000 },
-        { id: 5, text: 'PB', value: 1000000000000000 },
-      ],
+      fileSizeNum: 0,
+      units: {
+        byte: Math.pow(10, 0),
+        KB: Math.pow(10, 3),
+        MB: Math.pow(10, 6),
+        GB: Math.pow(10, 9),
+        TB: Math.pow(10, 12),
+        PB: Math.pow(10, 15) 
+      },
     }
   },
+  methods: {
+  },
+  computed: {
+    fileSizeText () {
+      if ( '' === this.fileSizeNum || typeof this.fileSizeNum === 'string'){
+        return '0 byte'
+      } else if (Number.MAX_SAFE_INTEGER < this.fileSizeNum){
+        return '扱える数値範囲を超えました。'
+      } else if (this.units.PB <= this.fileSizeNum) {
+        return `${(this.fileSizeNum / this.units.PB).toFixed(2)} PB`
+      } else if (this.units.TB <= this.fileSizeNum) {
+        return `${(this.fileSizeNum / this.units.TB).toFixed(2)} TB`
+      } else if (this.units.GB <= this.fileSizeNum) {
+        return `${(this.fileSizeNum / this.units.GB).toFixed(2)} GB`
+      } else if (this.units.MB <= this.fileSizeNum) {
+        return `${(this.fileSizeNum / this.units.MB).toFixed(2)} MB`
+      } else if (this.units.KB <= this.fileSizeNum) {
+        return `${(this.fileSizeNum / this.units.KB).toFixed(2)} KB`
+      } 
+      return `${this.fileSizeNum} byte`
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.file-size-text {
+  text-align: center;
+  font-size: 4em;
+}
 .file-size-input {
   display: flex;
   align-items: baseline;
   justify-content: center;
-}
-@mixin not-outline {
-  outline: none;
-  &:hover {
-    outline: none;
-    background-color: aquamarine;
-  }
-  &:active {
-    outline: none;
-  }
 }
 .file-size-label {
   font-size: 1.4em;
   padding: 20px;
   text-align: center;
 }
-
 .num-input {
-  @include not-outline;
-  height: 80px;
-  width: 200px;
-  font-size: 4.5em;
-  text-align: right;
+  width: 400px;
+  height: 3em;
+  font-size: 1em;
+  text-align: center;
   margin: 50px 0;
+  background-color: aliceblue;
+  border: solid 2px blue;
 }
 input[type='number']::-webkit-inner-spin-button,
 input[type='number']::-webkit-outer-spin-button {
