3ab23c035d4bf95999882eb099dc72e64c2923d2
furukawaTakumi <black3moon88@gmail.com>
2020-09-09 22:03:43 +0900

    [追加]状態によって見た目が変化するボタンを作成

---
+++ components/StateFullButtons.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-btn
+    @click="$emit('click', isAccept)"
+    :color="colorString"
+    rounded
+    dark
+    class="state-full-btn"
+    :class="{ 'is-accept-color': isAccept, 'is-not-accept-color': !isAccept }"
+  >
+    {{ textString }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      state: { isAccept: true, text: 'Next' },
+      stateString: 'Next',
+      isAccept: true,
+    }
+  },
+  methods: {
+    changeAcceptState() {
+      this.isAccept = true
+    },
+    changeRejectState() {
+      this.isAccept = false
+    },
+  },
+  computed: {
+    colorString() {
+      if (this.isAccept) {
+        return 'teal'
+      } else {
+        return 'error'
+      }
+    },
+    textString() {
+      if (this.isAccept) {
+        return 'Next'
+      } else {
+        return '---'
+      }
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.state-full-btn {
+  width: 100px;
+}
+.is-accept-color {
+  background-color: chartreuse;
+}
+.is-not-accept-color {
+  background-color: yellow;
+}
+</style>
